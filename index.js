98b151cba0af15d3bb1d8d68d74b0d574a85fb44
trajkd <trajkd@gmail.com>
2020-09-14 17:52:42 +0200

    fix: move js to html page as a script

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var groveSensor = require('jsupm_grove');
 var temp = new groveSensor.GroveTemp(1);
 
-document.getElementById("temp").innerHTML = "The current temperature is C " + temp.value();
-
 var localtunnel = require('localtunnel');
 var port = 8081;
 
